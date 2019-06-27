b987b32d220dd279f34d0a828ff669f78c67fd19
Graham Neubig <neubig@gmail.com>
2019-06-27 16:02:17 -0400

    Updated do_tokenize

--- create-alignments/do_tokenize.py
+++ create-alignments/do_tokenize.py
@@ -2,9 +2,24 @@ import spacy
 import re
 import sys
 
+# First install spacy and download models
+# $ pip install spacy
+# $ python -m spacy download en_core_web_sm
+# $ python -m spacy download fr_core_news_sm
+
+# Then run by doing the following for English or French respectively:
+# $ python do_tokenize.py en < in.txt > out.txt
+# $ python do_tokenize.py fr < in.txt > out.txt
+
 space_re = re.compile(r'  +')
 
-nlp = spacy.load(sys.argv[1], disable=["parser", "tagger", "ner"])
+lang = sys.argv[1]
+models = {
+  'en': 'en_core_web_sm',
+  'fr': 'fr_core_news_sm',
+}
+
+nlp = spacy.load(models[lang], disable=["parser", "tagger", "ner"])
 for line in sys.stdin:
   line = re.sub(space_re, ' ', line.strip())
   doc = nlp(line)
