0c6028151f3bb74a7a2f8004fe01bf826f261903
Graham Neubig <neubig@gmail.com>
2019-06-27 11:42:09 -0400

    Update to alignment script to go in proper order

--- create-alignments/make_alignments.py
+++ create-alignments/make_alignments.py
@@ -34,9 +34,9 @@ f_etrg = [f'wmt2019-system-data/mtnt/mtnt-test2019.{lang}-en.{lang}.tok' for f i
 f_all = f_orig + f_ftrg + f_etrg
 e_all = e_orig + e_ftrg + e_etrg
 
-# run_cmd(f'mkdir -p alignments/{lang}')
-# run_cmd('cat '+' '.join(f_all)+f' | sed "s/^$/__EMPTY__/g" > alignments/{lang}/all.{lang}')
-# run_cmd('cat '+' '.join(e_all)+f' | sed "s/^$/__EMPTY__/g" > alignments/{lang}/all.en')
+run_cmd(f'mkdir -p alignments/{lang}')
+run_cmd('cat '+' '.join(f_all)+f' | sed "s/^$/__EMPTY__/g" > alignments/{lang}/all.{lang}')
+run_cmd('cat '+' '.join(e_all)+f' | sed "s/^$/__EMPTY__/g" > alignments/{lang}/all.en')
 
 if args.aligner == 'giza':
   run_cmd(f'{args.mdir}/scripts/training/train-model.perl -external-bin-dir {mdir}/tools -last-step 4 -root-dir alignments/{lang}/moses -corpus alignments/{lang}/all -f {lang} -e en -alignment grow-diag-final-and -cores 4 -parallel')
@@ -58,18 +58,21 @@ else:
 # TODO: figure out what GIZA++ outputs
 alignfile = None if args.aligner == 'giza' else f'alignments/{lang}/fa/all.align.{lang}-en'
   
-def assign_alignments(aligns, inf, outf):
+def assign_alignments(aligns, inf, outf, reverse=False):
   print(f'assign_alignments(aligns, {inf}, {outf})')
   if inf == outf:
     raise ValueError(f'Error {inf} == {outf}')
   with open(inf, 'r') as ins, open(outf, 'w') as outs:
     for line in ins:
-      print(aligns.readline().strip(), file=outs)
+      if not reverse:
+        print(aligns.readline().strip(), file=outs)
+      else:
+        print(' '.join([f'{trg}-{src}' for (src,trg) in [v.split('-') for v in aligns.readline().strip().split(' ')]]), file=outs)
 
 with open(alignfile, 'r') as alignstream:
   for f in f_orig:
     assign_alignments(alignstream, f, f.replace(f'{lang}.tok', f'{lang}-en-{args.aligner}align'))
   for f in f_ftrg:
-    assign_alignments(alignstream, f, f.replace(f'{lang}.tok', f'{lang}-en-{args.aligner}align'))
+    assign_alignments(alignstream, f, f.replace(f'{lang}.tok', f'en-{lang}-{args.aligner}align'), reverse=True)
   for e in e_etrg:
     assign_alignments(alignstream, e, e.replace(f'en.tok', f'{lang}-en-{args.aligner}align'))
